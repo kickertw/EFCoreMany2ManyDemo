3069c793f5eb805d60ccf57735c2b4e27fca68e9
twong <twong@reliaslearning.com>
2021-01-25 22:54:28 -0500

    Creation of EF many to many relationship with minimal setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreMany2ManyDemoConsole.Data;
+using EFCoreMany2ManyDemoConsole.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreMany2ManyDemoConsole
 {
@@ -6,7 +12,71 @@ namespace EFCoreMany2ManyDemoConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SeedData();
+            var students = GetStudents();
+
+            foreach (var student in students)
+            {
+                var courseList = string.Join(',', student.Courses.Select(i => i.Title));
+                Console.WriteLine($"{student.Name} is in courses [{courseList}]");
+            }
+        }
+
+        static List<Student> GetStudents()
+        {
+            using var db = new MyContext();
+            return db.Students
+                .Include(i => i.Courses)
+                .ToList();
+        }
+
+        static void SeedData()
+        {
+            using var db = new MyContext();
+            if (db.Students.Any())
+            {
+                return;
+            }
+
+            var seedStudents = new List<Student>
+            {
+                new Student
+                {
+                    Name = "Hope"
+                },
+                new Student
+                {
+                    Name = "Faith"
+                },
+                new Student
+                {
+                    Name = "Grace"
+                }
+            };
+
+            var seedCourses = new List<Course>
+            {
+                new Course
+                {
+                    Title = "Discrete Mathematics"
+                },
+                new Course
+                {
+                    Title = "Relational Databases"
+                },
+                new Course
+                {
+                    Title = "Intro to Comp Sci"
+                },
+            };
+
+            seedStudents[0].Courses = seedCourses;
+            seedStudents[1].Courses = seedCourses.GetRange(1, 2);
+            seedStudents[2].Courses = seedCourses.GetRange(2, 1);
+
+            db.Students.AddRange(seedStudents);
+            db.Courses.AddRange(seedCourses);
+            db.SaveChanges();
         }
     }
 }
